d2500adfb4672bc341540819fce237566e9a316c
Igor <igor.danilov1824@gmail.com>
2021-05-06 14:01:11 +0300

    update

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {View} from "./features/settings/template/View";
+
+ReactDOM.render(
+  <React.StrictMode>
+      <View />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
