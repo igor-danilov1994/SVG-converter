00285d641b381489b82893960a914bec1098844f
Igor <igor.danilov1824@gmail.com>
2021-05-06 13:56:53 +0300

    update

--- my-app/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './App.css';
-import { View } from './features/settings/organisms/View';
-
-export function App() {
-  <View />
-}
--- my-app/src/features/settings/atoms/ShowIcon.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-type ShowIconType = {
-  svgHTML: any;
-};
-
-export const ShowIcon: React.FC<ShowIconType> = ({ svgHTML }) =>
-    <div style={{ marginLeft: '20px' }} dangerouslySetInnerHTML={{ __html: svgHTML }}/>
-
